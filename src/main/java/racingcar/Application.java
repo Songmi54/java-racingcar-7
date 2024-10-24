ab0170de93709b359ebdcd57d3ecf8041f1ffd79
Songmi54 <Songmi0054@gmail.com>
2024-10-25 02:13:39 +0900

    feat(input): add user input handling logic (car names, attempt count) >> - inputcarnames에 입력을 받아 쉼표를 기준으로 구분하여 carNames 배열에 저장 >> - carNames가 5자 이하인지 확인하는 예외처리 구현 >> - attemptCount에 시도할 횟수 입력받음

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,55 @@
 package racingcar;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+
+
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO: 프로그램 구현
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Random rand = new Random();
+        String separator = "[,]+";
+        int trynum = 0;
+
+
+
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+
+        String inputcarnames = br.readLine();
+        String[] carnames = inputcarnames.split(separator);
+        //System.out.println("입력받은 차 이름들 " + inputcarnames);
+        //for (String carname : carnames) {System.out.println("배열) car name: " + carname);}
+
+        // 이름이 5자 이하인지 확인
+        for (String carname : carnames) {
+            if (carname.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+            }
+        }
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        trynum = Integer.parseInt(br.readLine());
+        //System.out.println(trynum);
+
+        System.out.println("\n실행 결과");
+
+        for (int i = 0; i < trynum; i++) {
+            int forwardmovement = 0;
+            for (String carname : carnames) {
+                if (rand.nextInt(10) >= 4) {
+                    forwardmovement++;
+                }
+                System.out.println(carname + " : " + forwardmovement);
+            }
+            System.out.println();
+
+        }
+
+        System.out.println("최종 우승자: ");
+
     }
 }
+
